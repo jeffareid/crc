0bda427a76a051e2565df73ff7781572cadac667
Jeff Reid <jeffareid@cox.net>
2025-09-01 23:48:42 -0700

    Fast non-reflected 64 bit CRC avx512
    Fast non-reflected 64 bit CRC avx512

---
+++ crz64f/crz64fg.cpp
@@ -0,0 +1,64 @@
+// crc64fg.cpp = generate constant table
+
+#include <iostream>
+#include <iomanip>
+#include <stdio.h>
+
+#if 1
+#define rk08 0x000000000000001Bull  // crc64 iso
+#else
+#define rk08 0x42F0E1EBA9EA3693ull  // crc64 ecma
+#endif
+
+static uint64_t prk[23] =
+    { 0* 0,64* 2,64* 3,64*16,64*17,64* 2,64* 1,
+      0* 0, 0* 0,64*14,64*15,64*12,64*13,64*10,
+     64*11,64* 8,64* 9,64* 6,64* 7,64* 4,64* 5,
+     64*32,64*33};
+
+uint64_t grk07(void)
+{
+uint64_t Nhi = 0x0000000000000001ull;
+uint64_t Nlo = 0x0000000000000000ull;
+uint64_t Q = 0ull;
+    for(size_t i = 0; i < 65; i++){
+        Q <<= 1;
+        if(Nhi){
+            Q |= 1;
+            Nlo ^= rk08;
+        }
+        Nhi = Nlo>>63;
+        Nlo <<= 1;
+    }
+    return Q;                       // 2^128/poly
+}
+
+uint64_t grk(uint64_t E){
+uint64_t N = 0x8000000000000000ull;
+    if (E <= 64)
+        return 0ull;
+    E -= 63;
+    for(size_t i = 0; i < E; i++)
+        N = (N<<1)^((0x00ul-(N>>63))&rk08);
+    return N;                       // 2^(E)%poly
+}
+
+int main(int argc, char**argv)
+{
+    uint64_t crk[23];
+	uint64_t i;
+    crk[0] = 0ull;                  // crk[0] not used
+    for(size_t i = 1; i < 23; i++)
+        crk[i] = grk(prk[i]);
+    crk[7] = grk07();               // rk07 = 2^128 / poly
+    crk[8] = rk08;                  // rk08 = poly-2^64
+    for(i = 21; i < 23; i++)
+        printf("rk_%01llu    dq      0x%016llx\n", i-20, crk[i]);
+    for (i = 1; i < 21; i++)
+        printf("rk%02llu    dq      0x%016llx\n", i, crk[i]);
+    for (i = 1; i < 3; i++)
+        printf("        dq      0x%016llx\n", crk[i]);
+    for (i = 1; i < 3; i++)
+        printf("        dq      0x%016llx\n", 0x0llu);
+    return(0);
+}
